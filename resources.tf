7cdc21b656e45545d4d1518ac603b179e14505fc
siddhardh palepu <siddharthshankar1993@gmail.com>
2018-08-11 20:27:59 +0000

    Added networking/security resources

--- resources.tf
+++ resources.tf
@@ -6,3 +6,28 @@ resource "aws_vpc" "environment-tf-example" {
 		Name = "terraform-aws-vpc-example"
 	     }
 }
+resource "aws_subnet" "tf_subnet1" {
+	cidr_block = "${cidrsubnet(aws_vpc.environment-tf-example.cidr_block, 3, 1)}"
+	vpc_id = "${aws_vpc.environment-tf-example.id}"
+	availability_zone = "us-east-1a"
+
+}
+resource "aws_subnet" "tf_subnet2" {
+        cidr_block = "${cidrsubnet(aws_vpc.environment-tf-example.cidr_block, 2, 2)}"
+        vpc_id = "${aws_vpc.environment-tf-example.id}"
+        availability_zone = "us-east-1b"
+
+}
+resource "aws_security_group" "tf_subnet_security" {
+	vpc_id = "${aws_vpc.environment-tf-example.id}"
+	
+	ingress {
+		cidr_blocks = [
+			"${aws_vpc.environment-tf-example.cidr_block}"
+			      ]
+		from_port = 80
+		to_port = 80
+		protocol = tcp
+	        }
+
+}
